f8441c925e27f03939defc55cb03759110ec0675
Michael Corrieri <mlcorrieri@gmail.com>
2021-02-08 20:07:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,5 @@
 def fizzbuzz(int)
-#   if int % 3 == 0
-#     puts "Fizz"
-#   elsif int % 5 == 0
-#     puts "Buzz"
-#   elsif (int % 3 == 0) && (int % 5 == 0)
-#     puts "FizzBuzz"
-#   end
-# end
+
 if int % 15 == 0
    "FizzBuzz"
 elsif int % 3 == 0
